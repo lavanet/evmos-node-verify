27ece54835a7394be67c541ab5e79ad4d5534fe0
nir tayeb <nir@lavanet.xyz>
2023-09-14 18:55:00 +0300

    change pruning settings

--- verify-evmos.py
+++ verify-evmos.py
@@ -2,7 +2,7 @@ import argparse
 from urllib.request import urlopen, Request
 import json
 
-PRUNING = 100
+PRUNING = 50000
 
 HEADERS = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
